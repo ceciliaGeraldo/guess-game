697efd69ee1700a069ca2cb74015f0f11af70388
Cecy Geraldo <cecygeraldo@Cecy-Macbook-Pro-tw.local>
2022-06-25 23:35:47 -0400

    feat: create header to main screen

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, SafeAreaView } from 'react-native';
+import { styles } from './styles';
+import { Header } from './components';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Header title='Adivina el número' />
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/header/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 96,
+    marginTop: 50,
+    backgroundColor: '#D9DBF1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#212121',
+    fontSize: 20,
+  },
+});
